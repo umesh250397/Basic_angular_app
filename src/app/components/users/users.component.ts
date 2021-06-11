cedd2d6d57ebce869afaaf1bc69d37f54cd4cf80
Umesh <umesh250397@gmail.com>
2021-06-11 15:02:57 +0530

    basic functionality

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -8,8 +8,11 @@ import { User } from '../../models/User';
 })
 export class UsersComponent implements OnInit {
   users : User[];
-  showExtended : Boolean = true;
-  loaded : Boolean = false;
+  showExtended : boolean = true;
+  loaded : boolean = false;
+  enableAdd: boolean = true;
+  // currentClasses = {};
+  // currentStyles = {};
 
   constructor() { }
 
@@ -19,12 +22,16 @@ export class UsersComponent implements OnInit {
       {
         firstName : 'John',
         lastName : 'Doe',
-        age : 30,
+        age : 70,
         address : {
           street: '50 Main st',
           city: 'Boston',
           state: 'MA'
-        }
+        },
+        // image: 'http://lorempixel.com/600/600/people/3',
+        isActive: true,
+        // balance: 100,
+        registered: new Date('01/02/2018 08:30:00')
       },
       {
         firstName : 'Kevin',
@@ -34,7 +41,11 @@ export class UsersComponent implements OnInit {
           street: '20 School st',
           city: 'Lynn',
           state: 'MA'
-        }
+        },
+        // image: 'http://lorempixel.com/600/600/people/2',
+        isActive: false,
+        // balance: 200,
+        registered: new Date('03/11/2017 06:20:00')
       },
       {
         firstName : 'karen',
@@ -44,27 +55,59 @@ export class UsersComponent implements OnInit {
           street: '55 Mill st',
           city: 'Miami',
           state: 'FL'
-        }
+        },
+        // image: 'http://lorempixel.com/600/600/people/1',
+        isActive: true,
+        // balance: 50,
+        registered: new Date('11/02/2016 10:30:00')
       }
     ];
 
     this.loaded = true;
     // this.showExtended = false;
 
-    this.addUser({
-      firstName : 'David',
-      lastName : 'Jackson',
-      // age : 44,
-      // address : {
-      //   street: '12 Wake st',
-      //   city: 'Miami',
-      //   state: 'FL'
-      // }
-    });
+    // this.addUser({
+    //   firstName : 'David',
+    //   lastName : 'Jackson',
+    //   // age : 44,
+    //   // address : {
+    //   //   street: '12 Wake st',
+    //   //   city: 'Miami',
+    //   //   state: 'FL'
+    //   // }
+    // });
+
+    // this.setCurrentClassed();
+    // this.setCurrentStyles();
   }
 
   addUser(user: User) {
     this.users.push(user);
   }
 
+  // setCurrentClassed() {
+  //   this.currentClasses = {
+  //     'btn-success' : this.enableAdd,
+  //     'big-text' : this.showExtended
+  //   }
+  // }
+
+  // setCurrentStyles() {
+  //   this.currentStyles = {
+  //     'padding-top' : this.showExtended ? '0' : '40px',
+  //     'font-size' : this.showExtended ? '' : '40px'
+  //   }
+  // }
+
 }
+
+//property binding --> [src]="var" or [disabled]="condition/boolean"    //recomended
+//property binding --> bind-src="var" or bind-disabled="condition/boolean"
+//property binding --> src="{{var}}" or disabled="{{condition/boolean}}"
+
+//class binding --> [class.class_name]="condition/boolean"
+//style binding --> [style.border]="condition/boolean"
+
+//ngNonBindable makes the html tag not bind values
+
+//Pipes , default pipes --> uppercase, lowercase, currency, date, number, percent
--- src/app/models/User.ts
+++ src/app/models/User.ts
@@ -1,10 +1,14 @@
 export interface User {
-  firstName : string;
-  lastName : string;
-  age ?: number; //makes it optional
+  firstName : string,
+  lastName : string,
+  age ?: number, //makes it optional
   address ?: {
     street?: string,
     city?: string,
     state?: string
-  }
+  },
+  // image?: string,
+  isActive?: boolean,
+  // balance?: number,
+  registered?: any
 }
